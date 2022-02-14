55639e80caefbfedf37936b6ddd52e0a7add2a44
ilnur.nasibullin <nasosurulya@gmail.com>
2022-02-14 23:44:45 +0500

    changes 3 steo

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -11,29 +11,21 @@ def generate_diff(filepath1, filepath2):
     json1 = json.load(open(filepath1))
     json2 = json.load(open(filepath2))
 
-    resultdict = dict()
+    result_str = '{\n'
 
     for key, value in json1.items():
         if key in json2:
             if json1[key] == json2[key]:
-                resultdict['{}'.format(JSON_ENCODE(key, value))] = ' '
+                result_str = result_str + '  {} {}'.format(' ', JSON_ENCODE(key, value)) + '\n'
             else:
-                resultdict['{}'.format(JSON_ENCODE(key, value))] = '-'
-                resultdict['{}'.format(JSON_ENCODE(key, json2[key]))] = '+'
+                result_str = result_str + '  {} {}'.format('-', JSON_ENCODE(key, value)) + '\n'
+                result_str = result_str + '  {} {}'.format('+', JSON_ENCODE(key, json2[key])) + '\n'
         else:
-            resultdict['{}'.format(JSON_ENCODE(key, value))] = '-'
+            result_str = result_str + '  {} {}'.format('-', JSON_ENCODE(key, value)) + '\n'
 
     for key, value in json2.items():
         if key not in json1:
-            resultdict['{}'.format(JSON_ENCODE(key, value))] = '+'
-
-    result_str = '{\n'
-
-    list_keys = list(resultdict.keys())
-    list_keys.sort()
-
-    for key in list_keys:
-        result_str = result_str + '    {} {}'.format(resultdict[key], key) + '\n'
-    result_str = result_str + '}'
-    return result_str
+            result_str = result_str + '  {} {}'.format('+', JSON_ENCODE(key, value)) + '\n'
+    
+    return result_str + '}'
 
