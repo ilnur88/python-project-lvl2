495abda293a8aae13fc1c97d82eabd88217f7510
ilnur.nasibullin <nasosurulya@gmail.com>
2022-02-21 23:25:54 +0500

    3 step

---
+++ tests/test_gendiff.py
@@ -0,0 +1,23 @@
+from gendiff.gendiff import generate_diff
+import pytest
+
+
+@pytest.fixture
+def file1():
+    return 'tests/fixtures/file1.json'
+
+
+@pytest.fixture
+def file2():
+    return 'tests/fixtures/file2.json'
+
+
+@pytest.fixture
+def answer():
+    return 'tests/fixtures/answer.json'
+
+
+def test_gendif_json(file1, file2, answer):
+    answer_file = open(answer, 'r')    
+    answer_str = answer_file.read()
+    assert generate_diff(file1, file2) == '{\n    host : hexlet.io\n  - timeout : 50\n  + timeout : 20\n  - proxy : 123.234.53.22\n  - follow : false\n  + verbose : true\n}'
