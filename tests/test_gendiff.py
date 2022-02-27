e2457d2f7bf9b1cdfa3aab691c64029c744c1dd3
ilnur.nasibullin <nasosurulya@gmail.com>
2022-02-27 22:53:07 +0500

    fix lint

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -21,22 +21,25 @@ def answer():
 def answer_plain():
     return 'tests/fixtures/answer_plain.txt'
 
+
 @pytest.fixture
 def answer_json():
     return 'tests/fixtures/answer_json.json'
 
 
 def test_gendif_tree(file1, file2, answer):
-    answer_file = open(answer, 'r')    
+    answer_file = open(answer, 'r')
     answer_str = answer_file.read()
     assert generate_diff(file1, file2) == answer_str
 
+
 def test_gendif_plain(file1, file2, answer_plain):
-    answer_file = open(answer_plain, 'r')    
+    answer_file = open(answer_plain, 'r')
     answer_str = answer_file.read()
     assert generate_diff(file1, file2, 'plain') == answer_str
 
+
 def test_gendif_json(file1, file2, answer_json):
-    answer_file = open(answer_json, 'r')    
+    answer_file = open(answer_json, 'r')
     answer_str = answer_file.read()
-    assert generate_diff(file1, file2, 'json') == answer_str    
+    assert generate_diff(file1, file2, 'json') == answer_str
